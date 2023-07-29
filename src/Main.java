1a5f1a724e8dbef048761a3c66b18e9bdb4815e0
Svetlana Soboleva <blankor@mail.ru>
2023-07-29 22:59:03 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 13676; // стоимость билета
+        int y = 20; // стоимость одной бонусной мили
+        int z = x / y; // количество бонусных миль за купленный билет
+        System.out.println("За купленный билет начислено " + z + " миль(я/и).");
+    }
+}
