01bea59d5b036987172e83a4e5070c08e53e2e50
Svetlana Soboleva <blankor@mail.ru>
2023-08-18 00:41:32 +0400

    change2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 13676; // стоимость билета
-        int mile_price = 20; // стоимость одной бонусной мили
-        int bonus_miles = ticket_price / mile_price; // количество бонусных миль за купленный билет
-        System.out.println("За купленный билет начислено " + bonus_miles + " миль(я/и).");
+        int ticketPrice = 13676; // стоимость билета
+        int milePrice = 20; // стоимость одной бонусной мили
+        int bonus = ticketPrice / milePrice; // количество бонусных миль за купленный билет
+        System.out.println("За купленный билет начислено " + bonus + " миль(я/и).");
     }
 }
